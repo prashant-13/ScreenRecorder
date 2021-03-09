10151b05dcd37648db747a7340e1e369f5d6ae96
Prashant <prashant.dange@healthcoco.com>
2021-03-09 19:02:42 +0530

    Added some changes

--- app/src/main/java/com/example/screenrecorder/RecordingActivity.java
+++ app/src/main/java/com/example/screenrecorder/RecordingActivity.java
@@ -29,10 +29,13 @@ public class RecordingActivity extends AppCompatActivity {
 
         if (isStoragePermissionGranted())
             startActivityForResult(mProjectionManager.createScreenCaptureIntent(), PERMISSION_CODE);
-
         if (isStoragePermissionGranted())
-            Toast.makeText(this, "Permission Granted", Toast.LENGTH_SHORT).show();
-    }
+            startActivityForResult(mProjectionManager.createScreenCaptureIntent(), PERMISSION_CODE);
+        if (isStoragePermissionGranted())
+            startActivityForResult(mProjectionManager.createScreenCaptureIntent(), PERMISSION_CODE);
+        if (isStoragePermissionGranted())
+            startActivityForResult(mProjectionManager.createScreenCaptureIntent(), PERMISSION_CODE);
+       }
 
     @Override
     public void onDestroy() {
@@ -89,8 +92,10 @@ public class RecordingActivity extends AppCompatActivity {
             Log.v(TAG, "Permission: " + permissions[0] + "was " + grantResults[0]);
             //resume tasks needing this permission
             startActivityForResult(mProjectionManager.createScreenCaptureIntent(), PERMISSION_CODE);
-
-            Toast.makeText(this, "Persmission Granted and screen recording started", Toast.LENGTH_SHORT).show();
+            startActivityForResult(mProjectionManager.createScreenCaptureIntent(), PERMISSION_CODE);
+            startActivityForResult(mProjectionManager.createScreenCaptureIntent(), PERMISSION_CODE);
+            startActivityForResult(mProjectionManager.createScreenCaptureIntent(), PERMISSION_CODE);
+            startActivityForResult(mProjectionManager.createScreenCaptureIntent(), PERMISSION_CODE);
         }
     }
 }
